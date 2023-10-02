01acc12fdeba35ed9d1a261d8fb3673c19c775a9
Luis Cardona <69089919+louieLouie117@users.noreply.github.com>
2023-10-02 16:27:02 -0500

    TestMobieUpdate

---
+++ Practice/FreeCodingBootcamp.js
@@ -0,0 +1,20 @@
+const remainder = 11 % 3;
+
+
+// uoting Strings with Single 
+
+
+
+//  Word Blank 
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "I saw a " + myAdjective + " ugly " + myNoun + " and I " + myVerb + " away from from it " + myAdverb ; // Change this line
+// Only change code above this line
+
+
+
+// Store Multiple Values in one Variable using JavaScript Arrays
