3303d0091e868b01b5a0b0f51c0d6d937ca63a31
Luis Cardona <cardona-luis@outlook.com>
2023-10-09 15:17:32 -0500

    binarySearch

--- AlgoExpert/arrays.js
+++ AlgoExpert/arrays.js
@@ -1,4 +1,14 @@
-// Transpose Matrix solution 1
+// Transpose Matrix --------------------------------------------------------------
+/*
+    You're given a 2D array of integers matrix. Write a function
+    that returns the transpose of the matrix. The transpose of a matrix is a flipped version of the original matrix across
+    its main diagonal (which runs from top-left to bottom-right); it switches
+    the row and column indices of the original matrix.
+    You can assume the input matrix always has at least 1 value; however its
+    width and height are not necessarily the same.
+  
+   */
+//  solution 1
 let sampleTrasnposeMarix = [
     [1,5,7],
     [3,4,6],
@@ -18,7 +28,7 @@ function transposeMatrix(matrix) {
     return transposeMatrix;
   }
 
-// Transpose Matrix solution 2 with Ai
+// Solution 2 with Ai
 function transposeMatrix(matrix) {
     return matrix[0].map((_, col) => matrix.map(row => row[col]));
-  }
+  }a
