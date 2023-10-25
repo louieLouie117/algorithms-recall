b09b67f98e9d8d6ec991188ea28a311f04e6809e
Luis Cardona <cardona-luis@outlook.com>
2023-10-25 08:47:36 -0500

    Branch Sum

--- AlgoExpert/binarySearchTrees.js
+++ AlgoExpert/binarySearchTrees.js
@@ -13,3 +13,56 @@
   
   */
 
+// Solution 1 ---------------------- iterative approach
+function findClosestValueInBst(tree, target) {
+  // Write your code here.
+  let currentNode = tree;
+  let closest = Infinity;
+  console.log(closest);
+
+  while (currentNode !== null) {
+    console.log("loop is running!")
+    if (Math.abs(target - closest) > Math.abs(target - currentNode.value)) {
+      closest = currentNode.value;      
+    }
+    if(target < currentNode.value){
+      currentNode = currentNode.left;
+    }
+    else if(target > currentNode.value){
+      currentNode = currentNode.right;
+    }
+    else{
+      break;
+    }
+    
+
+  }
+    console.log("loop stopped!", closest)
+    return closest
+  
+}
+
+// Solution 2 ------------------------------------- Recoursively
+function findClosestValueInBst(tree, target) {
+  // Write your code here.
+    return findClosestValueInBstHelper(tree, target, Infinity);
+
+}
+// This is a helper function that implements recursion
+function findClosestValueInBstHelper(currentNode, target, closest) {
+  // Base case: if the current node is null, return the closest value
+  if (currentNode === null) {
+    return closest;
+  }
+  // Recursive case: update the closest value if needed and move to the left or right subtree
+  if (Math.abs(target - closest) > Math.abs(target - currentNode.value)) {
+    closest = currentNode.value;
+  }
+  if (target < currentNode.value) {
+    return findClosestValueInBstHelper(currentNode.left, target, closest);
+  } else if (target > currentNode.value) {
+    return findClosestValueInBstHelper(currentNode.right, target, closest);
+  } else {
+    return closest;
+  }
+}
