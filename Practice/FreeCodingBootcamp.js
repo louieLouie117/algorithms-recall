a75eaafdb6a906a7edd0311a047602811fbc6f78
Luis Cardona <69089919+louieLouie117@users.noreply.github.com>
2023-10-27 16:34:51 -0500

    FromBrake

--- Practice/FreeCodingBootcamp.js
+++ Practice/FreeCodingBootcamp.js
@@ -529,7 +529,38 @@ function orderMyLogic(val) {
   }
 }
 
+// Golf Code 
+/* In the game of Golf, each hole has a par, meaning,
+ the average number of strokes a golfer is expected
+  to make in order to sink the ball in the hole to
+   complete the play. Depending on how far above or 
+   below par your strokes are, there is a different
+    nickname.*/
 
+    const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+    function golfScore(par, strokes) {
+      // Only change code below this line
+      if(strokes === 1){
+        return "Hole-in-one!"
+      }else if (strokes <= par-2){
+        return "Eagle"
+      } else if(strokes === par -1){
+        return "Birdie"
+      } else if (strokes === par){
+        return "Par";
+      } else if (strokes === par + 1){
+        return "Bogey";
+      } else if (strokes === par + 2){
+        return "Double Bogey"
+      } else if (strokes >= par +3){
+        return "Go Home!"
+      }
+    
+      // Only change code above this line
+    }
+    
+    golfScore(5, 4);
 
 
 
