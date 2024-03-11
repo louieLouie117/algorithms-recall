5260f2f93727d976a649a6b955ab7048b2ecf895
Luis Cardona <cardona-luis@outlook.com>
2024-03-11 14:09:55 -0500

    FindOdd

--- codeWars/2024March.js
+++ codeWars/2024March.js
@@ -1,3 +1,4 @@
+// 2/28/2024
 // Complete the solution so that the function will break up camel casing, using a space between words.
 
 
@@ -17,4 +18,28 @@ function solution(string) {
       console.log(newWord); // Output the modified string
   
       return newWord; // Return the modified string
+  }
+
+  /**
+   * 2/29/2024
+   * Given an array of integers, find the one that appears an odd number of times.
+   * There will always be only one integer that appears an odd number of times.
+   */
+  function findOdd(arr) {
+    //happy coding!
+    for (let i = 0; i < arr.length; i++) {
+      let count = 0; // Initialize a counter for each element
+  
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[i] === arr[j]) {
+          count++; // Increment the count if the elements match
+        }
+      }
+  
+      if (count % 2 !== 0) {
+        console.log("Odd-occurring element:", arr[i]);
+        return arr[i]
+      }
+      
+    }
   }
