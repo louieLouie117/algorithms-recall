c2ddda9ed371de8917fc9ee2e3663c98cd035632
Luis Cardona <cardona-luis@outlook.com>
2024-03-18 09:02:19 -0500

    FindSmallestInt

--- codeWars/2024March.js
+++ codeWars/2024March.js
@@ -241,3 +241,39 @@ function countVowels(str) {
   console.log(count);
   return count;
 }
+
+
+
+
+
+
+
+/** 3/18/2024
+Given an array of integers your solution should find the smallest integer.
+For example:
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+ */
+
+class SmallestIntegerFinder {
+   findSmallestInt(args) {
+    console.log(args)
+    
+    let smallest = args[0];
+    
+    for(let i = 0; i < args.length; i++){
+      console.log(args[i], " : ", smallest)
+      
+      if(args[i] < smallest){
+        // updated the smallest number if args[i] < smallest -----------------
+        smallest = args[i];
+      }
+    }
+    
+    console.log(smallest)
+    
+    // Return the smallest number
+    return smallest
+  }
+}
