3e2acf7934576351b31d0d63840e773e276f894d
Luis Cardona <cardona-luis@outlook.com>
2024-03-18 08:04:18 -0500

    countSheep

--- codeWars/2024March.js
+++ codeWars/2024March.js
@@ -115,3 +115,75 @@ The input string will only consist of lower case letters and/or spaces.
       console.log(count)
       return count;
     }
+
+
+    // This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+// 3/12
+// This was a fun alog and I need to remmber to use % to check if a number is even
+
+function simpleMultiplication(number) {
+  console.log("number", number)
+let result = 0;
+
+if (number % 2 === 0) {
+  console.log("The number is even.");
+  number = number * 8
+} else {
+  console.log("The number is odd.");
+  number = number * 9;  
+}
+console.log("result", number)
+return number;
+    
+}
+
+// Refactored no result varibale needed
+function simpleMultiplication(number) {
+  console.log("number", number);
+
+  if (number % 2 === 0) {
+      console.log("The number is even.");
+      number *= 8;
+  } else {
+      console.log("The number is odd.");
+      number *= 9;
+  }
+
+  console.log("result", number);
+  return number;
+}
+
+
+// 3/12/2024 
+// Find the the other angle
+// This was a smmple but fun alogos reay like the fundamentas 
+
+function otherAngle(a, b) {
+  console.log("Angel A", a)
+  console.log("Angel B", b)
+  let c = 180 - (a + b);
+  console.log(c)
+  return c;
+}
+
+// 3/12/2024
+function countSheep (num){
+  console.log("num", num)
+  let count = 0;
+  let stringArray = [];
+  if(num === 0){
+    return "";
+  }
+
+  let loopAmount = num;
+  for(let i = 1; i <= loopAmount; i++){
+    count += 1;
+    stringArray.push(`${count} sheep...`);
+  }
+  console.log("result", stringArray)
+  let resultSting = stringArray.join("");
+  console.log("resultSting", resultSting)
+  return resultSting;
+
+}
