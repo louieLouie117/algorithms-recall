e7b6e3bf8e80fcc0e327b67cf70fa0730c196684
Luis Cardona <69089919+louieLouie117@users.noreply.github.com>
2024-03-21 15:03:08 -0500

    isSquare

--- Practice/mobile.js
+++ Practice/mobile.js
@@ -25,3 +25,18 @@ function updateLight(current) {
 
 
 
+
+ 
+
+ /**
+  * 3/21/2024
+  * Given an integral number, determine if it's a square number:
+  */
+
+ function isSquare(){
+    console.log(n)
+    let thePerficSquare = n >= 0 && Math.sqrt(n) % 1 === 0;
+    return thePerficSquare
+  }
+  
+  
