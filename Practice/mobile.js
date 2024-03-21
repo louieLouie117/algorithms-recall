06664a2d2f0edb01c3294661151ed42a2b134da4
Luis Cardona <69089919+louieLouie117@users.noreply.github.com>
2024-03-21 09:18:14 -0500

    updateLight

--- Practice/mobile.js
+++ Practice/mobile.js
@@ -1,3 +1,27 @@
+// 3/21/2024
+/**
+ You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+
+ */
+
+
+function updateLight(current) {
+    if (current === "green"){
+         return "yellow"
+    }
+     else if (current === "yellow"){
+         return "red"
+     }
+     else if (current === "red"){
+         return "green"
+     }else {
+       console.log("erorr no statments are true")
+     }
+ 
+ }
 
 
 
