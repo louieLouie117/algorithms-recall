efa4d02e7e826f50b83aaafd5b698322f5ab8f07
Luis Cardona <69089919+louieLouie117@users.noreply.github.com>
2024-03-22 08:46:15 -0500

    betterThanAverage

--- Practice/mobile.js
+++ Practice/mobile.js
@@ -59,4 +59,38 @@ function updateLight(current) {
     let presentSheep = sheep.length - missingSheep;
     return presentSheep;
   }
+
+
+/**3/21/24
+
+There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+   */
+
+  function betterThanAverage(classPoints, yourPoints) {
+    // Your code here
+    console.log("My score", yourPoints);
+    console.log("ClassPoints", classPoints)
+  //   set var for numbers added
+    let sumNumbers = 0;
+  //   loop the array and add the number
+    for(let i =0; i < classPoints.length; i++){
+      console.log("loop is running", classPoints[i])
+      sumNumbers += classPoints[i]
+      
+    }
+    console.log("Sum Numbser", sumNumbers)
+    let average = sumNumbers / classPoints.length
+      console.log("Average", average)
+    if(yourPoints > average ){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  
   
